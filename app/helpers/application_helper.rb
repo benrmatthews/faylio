0a174696de50692cfe1d8dd82b6928da192851f6
benrmatthews <ben@brightone.org.uk>
2013-10-21 20:04:31 +0000

    Added redcarpet markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar img-circle")
   end
+  
+  def markdown
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    :autolink => true, :space_after_headers => true, :hard_wrap => true, :filter_html=> true, :no_intraemphasis => true, :fenced_code => true, :gh_blockcode => true)
+  end
 end
