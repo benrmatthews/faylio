edff8d882b57930f990085d6f9db7b50111f9e83
benrmatthews <ben@brightone.org.uk>
2013-11-20 20:18:06 +0000

    Changed fasil description and lesson from string to integer

--- db/migrate/20131018222318_create_fails.rb
+++ db/migrate/20131018222318_create_fails.rb
@@ -2,10 +2,10 @@ class CreateFails < ActiveRecord::Migration
   drop_table :fails
   def change
     create_table :fails do |t|
-      t.string :headline
-      t.text :description
-      t.text :lesson
-      t.string :link
+      t.string :headline, limit:nil
+      t.text :description, limit:nil
+      t.text :lesson, limit:nil
+      t.string :link, limit:nil
       t.integer :user_id
 
       t.timestamps
